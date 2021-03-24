4527e268887ce58d764ec4f087866acadd12c6b2
Maikon Canuto <maikoncanuto@gmail.com>
2021-03-24 16:38:50 -0300

    Alteracao do routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'ambientes',
+    loadChildren: () => import('./features/ambiente/ambiente.module').then(ambienteModule => ambienteModule.AmbienteModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
