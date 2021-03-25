84b83c3d08e18b9418e74b9f176f28e4a636d689
Maikon Canuto <maikoncanuto@gmail.com>
2021-03-25 09:56:25 -0300

    Criando layout

--- src/app/features/ambiente/ambiente.module.ts
+++ src/app/features/ambiente/ambiente.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AmbienteRoutingModule } from './ambiente-routing.module';
-import { AmbienteListComponent } from './components/ambiente-list/ambiente-list.component';
-import { AmbienteCreateComponent } from './components/ambiente-create/ambiente-create.component';
-import { AmbienteUpdateComponent } from './components/ambiente-update/ambiente-update.component';
-import { AmbienteDeleteComponent } from './components/ambiente-delete/ambiente-delete.component';
-import { AmbienteDeleteContainerComponent } from './containers/ambiente-delete-container/ambiente-delete-container.component';
-import { AmbienteListContainerComponent } from './containers/ambiente-list-container/ambiente-list-container.component';
-import { AmbienteUpdateContainerComponent } from './containers/ambiente-update-container/ambiente-update-container.component';
-import { AmbienteCreateContainerComponent } from './containers/ambiente-create-container/ambiente-create-container.component';
+import {AmbienteRoutingModule} from './ambiente-routing.module';
+import {AmbienteListComponent} from './components/ambiente-list/ambiente-list.component';
+import {AmbienteCreateComponent} from './components/ambiente-create/ambiente-create.component';
+import {AmbienteUpdateComponent} from './components/ambiente-update/ambiente-update.component';
+import {AmbienteDeleteComponent} from './components/ambiente-delete/ambiente-delete.component';
+import {AmbienteDeleteContainerComponent} from './containers/ambiente-delete-container/ambiente-delete-container.component';
+import {AmbienteListContainerComponent} from './containers/ambiente-list-container/ambiente-list-container.component';
+import {AmbienteUpdateContainerComponent} from './containers/ambiente-update-container/ambiente-update-container.component';
+import {AmbienteCreateContainerComponent} from './containers/ambiente-create-container/ambiente-create-container.component';
+import {FlexLayoutModule, FlexModule} from '@angular/flex-layout';
 
 
 @NgModule({
@@ -25,7 +26,10 @@ import { AmbienteCreateContainerComponent } from './containers/ambiente-create-c
   ],
   imports: [
     CommonModule,
-    AmbienteRoutingModule
+    AmbienteRoutingModule,
+    FlexLayoutModule,
+    FlexModule
   ]
 })
-export class AmbienteModule { }
+export class AmbienteModule {
+}
